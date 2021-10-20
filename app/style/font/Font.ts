048fbeabd536b5a69622dce0a6723cb72c3cac29
Erox1 <InfinityWaffles@outlook.com>
2021-10-20 12:49:30 -0700

    Added The Basic Styles, Colours, And Layout
    Added the basic styles and components for the text colours and layout constants. As well as adding prettier and eslint.

---
+++ app/style/font/Font.ts
@@ -0,0 +1,22 @@
+const FontSize = {
+  // Main Text
+  H1: 27.1,
+  H2: 13.6,
+  H3: 10.4,
+  H4: 7.7,
+
+  // Clickable Text
+  ClickableH1: 18.1,
+  ClickableH2: 13.6,
+  ClickableH3: 11.3,
+
+  // Tag Text
+  TagH1: 13.6,
+  
+  // Bar Text
+  BarH1: 16.7
+};
+
+const Font = { FontSize };
+
+export { Font };
---
+++ app/style/index.ts
@@ -0,0 +1,5 @@
+import { Colours } from './colours';
+import { Font } from './font';
+import { Layout } from './layout';
+
+export { Colours, Font, Layout };
