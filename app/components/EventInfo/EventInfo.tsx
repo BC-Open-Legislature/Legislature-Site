b2a3e0844395a2c2dd8f553af780865135aad657
Erox1 <InfinityWaffles@outlook.com>
2021-10-26 13:17:12 -0700

    Added Functionality To The Event Button
    Added the ability to add an on touch function to the event button.

--- app/components/EventInfo/EventInfo.tsx
+++ app/components/EventInfo/EventInfo.tsx
@@ -1,26 +1,29 @@
 import React from "react";
-import { View } from "react-native";
+import { TouchableHighlight, View } from "react-native";
 import StandardText from '../StandardText'
 import { Colours, Font, Layout } from '../../style/'
 import Tag from '../Tag'
 
 interface Props {
-  children: string,
+  children: string;
   type: (
     'Greens' | 'NDP' | 'BCLiberals' | 'OtherParty' | 'Independent' |
     'Yes' | 'No' | 'Other' | 'None'
-  )
+  );
+  onPress: any;
 }
 
 export default function EventInfo(props: Props): React.ReactElement {
   return (
-    <View>
-        <Tag type={ props.type } />
-        <View style={{ paddingLeft: Layout.Spacing.x1}}>
-            <StandardText colour={ Colours.Black[100] } fontSize={ Font.FontSize.H2 } >
-                {props.children}
-            </StandardText>
-        </View>
-    </View>
+    <TouchableHighlight onPress={props.onPress} underlayColor={ null }>
+      <View>
+          <Tag type={ props.type } />
+          <View style={{ paddingLeft: Layout.Spacing.x1}}>
+              <StandardText colour={ Colours.Black[100] } fontSize={ Font.FontSize.H2 } >
+                  {props.children}
+              </StandardText>
+          </View>
+      </View>
+    </TouchableHighlight>
   );
 }
