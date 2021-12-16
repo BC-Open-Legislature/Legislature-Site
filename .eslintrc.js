89df047e9d7f9124e8386d988036840ede0ccce4
Eroxl <InfinityWaffles@outlook.com>
2021-12-16 16:14:35 -0500

    Setup ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'airbnb',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 13,
+      sourceType: 'module',
+    },
+    plugins: [
+      'react',
+      '@typescript-eslint',
+    ],
+    rules: {
+      'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+      'no-use-before-define': 'off',
+      '@typescript-eslint/no-use-before-define': ['error'],
+      'import/order': [
+        'error',
+        {
+          groups: [['external', 'builtin'], 'internal', ['parent', 'sibling', 'index']],
+          'newlines-between': 'always',
+        },
+      ],
+      'import/extensions': 0,
+    },
+    settings: {
+      'import/resolver': {
+        node: {
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+      },
+    },
+  };
